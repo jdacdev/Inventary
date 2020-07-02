e1a8b268c9bc995ffd8d820d9ace1f2508037001
Juan Amaya <juan.amayacardenas@outlook.es>
2020-07-01 22:00:31 -0500

    Migration

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,11 +10,56 @@ namespace DataAccess
     public class InventaryContext : DbContext
     {
         public DbSet<ProductEntity> Products { get; set; }
-
         public DbSet<CategoryEntity> Categories { get; set; }
         public DbSet<StorageEntity> Storages { get; set; }
         public DbSet<WarehouseEntity> Warehouses { get; set; }
         public DbSet<InOutEntity> InOuts { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //Solo configuramos una cadena de conexion si no fue configurada antes
+            if (!options.IsConfigured)
+            {
+                //Item de la cadena de conexion
+                //Server  : Nombre de la instancia de base de datos / Ip .
+                //Database: Nombre de la base de datos a que vamos a acceder.
+                //User Id : Nombre de usuario para acceder a la instancia
+                //Password: Contraseña con la que se accede a la instancia.
+                options.UseSqlServer("Server=localhost; Database = InventaryDataBase; User Id = sa; Password=juan1234");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //realizamos la ejecucion original del metod
+            base.OnModelCreating(modelBuilder);
+
+            //cargar data en Category
+            modelBuilder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity { CategoryId = "ASH", CategoryName = "Aseo Hogar" },
+                new CategoryEntity { CategoryId = "ASP", CategoryName = "Aseo Personal" },
+                new CategoryEntity { CategoryId = "HGR", CategoryName = "Hogar" },
+                new CategoryEntity { CategoryId = "PRF", CategoryName = "Perfumería" },
+                new CategoryEntity { CategoryId = "SLD", CategoryName = "Salud" },
+                new CategoryEntity { CategoryId = "VDJ", CategoryName = "Video Juegos" }
+                );
+
+            //cargar data en Werehouseces
+            modelBuilder.Entity<WarehouseEntity>().HasData(
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Central", WarehouseAddress = "Calle 8 con 23" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Norte", WarehouseAddress = "Calle norte con occidente" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Alternativa", WarehouseAddress = "Calle falsa 123" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Secreta", WarehouseAddress = "Calle x con y" }
+                );
+
+            //cargar data en Products
+            modelBuilder.Entity<ProductEntity>().HasData(
+                new ProductEntity { ProductId = "ASJ-98745", ProductName = "Crema para manos marca Tersa", ProductDescription = "", CategoryId = "PRF" },
+                new ProductEntity { ProductId = "RPT-5465879", ProductName = "Pastillas para la garganta LESUS", ProductDescription = "", CategoryId = "SLD" }
+                );
+        }
+
 
     }
 }
---
+++ DataAccess/Migrations/20200702024625_InventaryMigration.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class InventaryMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(maxLength: 50, nullable: false),
+                    CategoryName = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warehouses",
+                columns: table => new
+                {
+                    WarehouseId = table.Column<string>(maxLength: 50, nullable: false),
+                    WarehouseName = table.Column<string>(maxLength: 100, nullable: false),
+                    WarehouseAddress = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouses", x => x.WarehouseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(maxLength: 50, nullable: false),
+                    ProductName = table.Column<string>(maxLength: 100, nullable: false),
+                    ProductDescription = table.Column<string>(maxLength: 600, nullable: true),
+                    TotalQuantity = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storages",
+                columns: table => new
+                {
+                    StorageId = table.Column<string>(maxLength: 50, nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    PartialQuantity = table.Column<int>(nullable: false),
+                    ProductId = table.Column<string>(nullable: true),
+                    WarehouseId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storages", x => x.StorageId);
+                    table.ForeignKey(
+                        name: "FK_Storages_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Storages_Warehouses_WarehouseId",
+                        column: x => x.WarehouseId,
+                        principalTable: "Warehouses",
+                        principalColumn: "WarehouseId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InOuts",
+                columns: table => new
+                {
+                    InOutId = table.Column<string>(maxLength: 50, nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    IsInput = table.Column<bool>(nullable: false),
+                    StorageId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InOuts", x => x.InOutId);
+                    table.ForeignKey(
+                        name: "FK_InOuts_Storages_StorageId",
+                        column: x => x.StorageId,
+                        principalTable: "Storages",
+                        principalColumn: "StorageId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "ASH", "Aseo Hogar" },
+                    { "ASP", "Aseo Personal" },
+                    { "HGR", "Hogar" },
+                    { "PRF", "Perfumería" },
+                    { "SLD", "Salud" },
+                    { "VDJ", "Video Juegos" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "3e70ce56-107c-4195-b52c-f9bccb8b1297", "Calle 8 con 23", "Bodega Central" },
+                    { "408588c4-d64d-4dc7-a579-8abd8b3e117a", "Calle norte con occidente", "Bodega Norte" },
+                    { "e7e544aa-7dbc-43a6-8e28-9f1c6fdb4aaa", "Calle falsa 123", "Bodega Alternativa" },
+                    { "00a0e122-845f-4152-90f8-7b8fac880cec", "Calle x con y", "Bodega Secreta" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "ProductDescription", "ProductName", "TotalQuantity" },
+                values: new object[] { "ASJ-98745", "PRF", "", "Crema para manos marca Tersa", 0 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "ProductDescription", "ProductName", "TotalQuantity" },
+                values: new object[] { "RPT-5465879", "SLD", "", "Pastillas para la garganta LESUS", 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InOuts_StorageId",
+                table: "InOuts",
+                column: "StorageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Storages_ProductId",
+                table: "Storages",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Storages_WarehouseId",
+                table: "Storages",
+                column: "WarehouseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InOuts");
+
+            migrationBuilder.DropTable(
+                name: "Storages");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Warehouses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
